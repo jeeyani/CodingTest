0f2eda73a638a978a6d9be17045cf40d678f2507
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-01-18 19:02:11 +0900

    코드업 기초 100제 - 데이터형

---
+++ src/codeupBasic100/dataType/n1028.java
@@ -0,0 +1,12 @@
+package codeupBasic100.dataType;
+
+import java.text.*;
+import java.util.*;
+
+public class n1028 {
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		
+	
+	}
+}
