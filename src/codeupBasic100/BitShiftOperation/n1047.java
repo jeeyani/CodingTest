6da5efa21866e61b6e4034b2439ea83de066c6fa
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-01-19 17:04:56 +0900

    기초-비트시프트연산

---
+++ src/codeupBasic100/BitShiftOperation/n1047.java
@@ -0,0 +1,13 @@
+package codeupBasic100.BitShiftOperation;
+
+import java.util.*;
+
+public class n1047 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		
+	    System.out.printf("%d",a<<1);
+	}
+}
---
+++ src/codeupBasic100/BitShiftOperation/n1048.java
@@ -0,0 +1,14 @@
+package codeupBasic100.BitShiftOperation;
+
+import java.util.*;
+
+public class n1048 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+	    System.out.printf("%d",a<<b);
+	}
+}
