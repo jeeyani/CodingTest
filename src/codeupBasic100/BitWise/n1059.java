7b8623f651f8685c9e7f03996fecf480942b96b4
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-01-19 17:52:40 +0900

    기초 - 비트단위논리연산

---
+++ src/codeupBasic100/BitWise/n1059.java
@@ -0,0 +1,16 @@
+package codeupBasic100.BitWise;
+
+import java.util.*;
+
+public class n1059 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		
+		System.out.print(~a);
+		
+		
+	}
+	
+}
---
+++ src/codeupBasic100/BitWise/n1060.java
@@ -0,0 +1,19 @@
+package codeupBasic100.BitWise;
+
+import java.util.*;
+
+public class n1060 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int result = a & b;
+		
+		System.out.print(result);
+		
+		
+	}
+	
+}
