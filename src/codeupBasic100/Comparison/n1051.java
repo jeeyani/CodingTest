6f5dd987ba5251cc6a00240ed54db75d5b858d04
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-01-19 17:15:36 +0900

    기초 - 비교연산

---
+++ src/codeupBasic100/Comparison/n1051.java
@@ -0,0 +1,19 @@
+package codeupBasic100.Comparison;
+
+import java.util.*;
+
+public class n1051 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(b>=a) {
+			System.out.print("1");
+		}
+		else {
+			System.out.print("0");
+		}
+	}
+}
