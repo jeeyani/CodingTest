7d4f8a352a7eb3507b512e495eae6dfa84a1334c
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-03-29 22:57:16 +0900

    백준 13305번_수정필요

---
+++ src/Baekjoon/Greedy/n13305.java
@@ -0,0 +1,64 @@
+package Baekjoon.Greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class n13305 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws Exception {
+		int test = Integer.parseInt(br.readLine());
+		List<Oils> list = inputValue(test);
+		System.out.println(solve(list));
+	}
+	
+	public static class Oils{
+		int km;
+		int price;
+		
+		public Oils(int km, int price) {
+			this.km = km;
+			this.price = price;
+		}
+		
+	}
+	
+	private static List<Oils> inputValue(int test) throws Exception {
+		List<Oils> list = new ArrayList<>();
+		String[] kmAdd=br.readLine().split(" ");
+		String[] priceAdd=br.readLine().split(" ");
+		
+		for (int i = 0; i < test-1; i++) {
+			list.add(new Oils(Integer.parseInt(kmAdd[i]), Integer.parseInt(priceAdd[i])));
+		}
+		return list;
+	}
+	
+	private static int solve(List<Oils> list) {
+		int totalPrice = 0;
+		int sum =0;
+		int minPrice = Integer.MAX_VALUE;
+		for(Oils oils:list) {
+			if(oils.price < minPrice) {
+				minPrice = oils.price;
+			}
+		}
+		
+		frist:for (int i = 0; i < list.size()-1; i++) {
+			if(list.get(i).price == minPrice) {
+				for (int j = i; j < list.size(); j++) {
+					sum+=list.get(j).km;
+				}
+				totalPrice +=list.get(i).price * sum;
+				break frist;
+			}else {
+				totalPrice +=list.get(i).price*list.get(i).km;
+			}
+		}
+		return totalPrice;
+	}
+
+}
