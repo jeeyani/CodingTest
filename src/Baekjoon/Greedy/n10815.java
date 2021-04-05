afbfc08c5b5972f7b35adff68270b54fb536dba4
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-04-05 23:10:31 +0900

    백준 10815번 시간초과

---
+++ src/Baekjoon/Greedy/n10815.java
@@ -0,0 +1,32 @@
+package Baekjoon.Greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class n10815 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	
+	public static void main(String[] args) throws Exception{
+		int user = Integer.parseInt(br.readLine());
+		String[] userCard = br.readLine().split(" ");
+		
+		int M = Integer.parseInt(br.readLine());
+		String[] mCard= br.readLine().split(" ");
+		
+		int[] result = new int[mCard.length];
+		for (int i = 0; i < mCard.length; i++) {
+			U: for (int j = 0; j < userCard.length; j++) {
+				if(userCard[j].equals(mCard[i])) {
+					result[i] = 1;
+					break U;
+				}
+			}
+		}
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]+ " ");
+		}
+	}
+
+}
