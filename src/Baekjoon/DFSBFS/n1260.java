a96d9ba49a9d5f116fe4af4fe4110e0fe53ff003
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-04-20 22:21:55 +0900

    백준 1260번 문제 수정

--- src/Baekjoon/DFSBFS/n1260.java
+++ src/Baekjoon/DFSBFS/n1260.java
@@ -2,7 +2,6 @@ package Baekjoon.DFSBFS;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -12,9 +11,8 @@ public class n1260 {
 	public static int m;
 	public static int start;
 	
-	public static boolean[] visiteDFS;
-	public static boolean[] visiteBFS;
-	public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+	public static boolean[] visited;
+	public static int[][] graph = new int[1001][1001];
 	
 	
 	public static void main(String[] args) throws Exception{
@@ -25,58 +23,65 @@ public class n1260 {
 		n = Integer.parseInt(input[0]);
 		m = Integer.parseInt(input[1]);
 		start = Integer.parseInt(input[2]);
-		
-		visiteDFS = new boolean[m+1];
-		visiteBFS = new boolean[m+1];
-		
-		for (int i = 0; i < m+1; i++) {
-			graph.add(new ArrayList<Integer>());
-		}
+				
+		visited = new boolean[1001];
 
 		//연결된 정보 입력받기
 		for (int i = 0; i < m; i++) {
-			String[] graphIn = br.readLine().split(" ");
-			graph.get(Integer.parseInt(graphIn[0])).add(Integer.parseInt(graphIn[1]));
+			String[] temp = br.readLine().split(" ");
+			int x = Integer.parseInt(temp[0]);
+			int y = Integer.parseInt(temp[1]);
+			
+			graph[x][y] = graph[y][x] = 1;
 		}
 		
 		DFS(start);
 		System.out.println("");
-		BFS(start);
+		
+		visited = new boolean[1001]; //초기화
+		BFS();
 		
 	} 
 
 
-	private static void BFS(int start) {
+	private static void BFS() {
 		
 		Queue<Integer> q = new LinkedList<Integer>();
 		
+		//시작점 체크
 		q.offer(start);
-		visiteBFS[start] = true;
+		visited[start] = true;
+		System.out.print(start+" ");
+		
 		
 		while(!q.isEmpty()) {
+			//값 먼저 꺼내기
 			int x = q.poll();
-			System.out.print(x+" ");
 			
-			for (int i = 0; i < graph.get(x).size(); i++) {
-				int y = graph.get(x).get(i);
-				if(!visiteBFS[y]) {
-					q.offer(y);
-					visiteBFS[y] = true;
+			for (int i = 1; i <= n; i++) {
+				//연결되어 있는 곳이고 방문하지 않은 곳이면 
+				if(graph[x][i] == 1 && visited[i] == false) {
+					q.offer(i);
+					visited[i] = true;
+					System.out.print(i+" ");
 				}
 			}
+			
+			
 		}
 		
 	}
 
 
 	private static void DFS(int start) {
-		visiteDFS[start] = true;
+		//방문표시
+		visited[start] = true;
 		System.out.print(start +" ");
 		
-		for (int i = 0; i < graph.get(start).size(); i++) {
-			int y = graph.get(start).get(i);
-			if(!visiteDFS[y]) {
-				DFS(y);
+		for (int i = 1; i <=n ; i++) {
+			//연결되어 있는 곳이고 방문하지 않은 곳이면 
+			if(graph[start][i] == 1 && visited[i] == false) {
+				DFS(i);
 			}
 		}
 	}
