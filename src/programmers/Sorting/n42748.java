42ed1b27bb91359770abe02944842582ebba254d
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-04-23 23:25:09 +0900

    프로그래머스 정렬 1번문제 수정완료

--- src/programmers/Sorting/n42748.java
+++ src/programmers/Sorting/n42748.java
@@ -9,7 +9,11 @@ public class n42748 {
 		
 		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
-		System.out.println(new Solution().solution(array, commands));
+		int[] result = new Solution().solution(array, commands);
+		
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i]);
+		}
 		
 	}
 
@@ -19,28 +23,32 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         //return 배열
         int[] answer = new int[commands.length];
-        
-        //자른 배열을 저장
-        int[] temp = {};
-        
-        for(int i=0; i<commands.length; i++){
+
+        for(int i=0; i<commands.length; i++) {
             
             int start = commands[i][0];
             int end = commands[i][1];
+            //자른 배열을 저장
+            int[] temp = new int[end-start+1];
             
-            for(int j=0; j<(start-end)+1; j++){
-                temp[j] = array[start-1];
-                start +=1;
+            int n = start;
+            for(int j=0; j<(end-start)+1; j++){
+                temp[j] = array[n-1];
+                n +=1;
                 
             }
             
             Arrays.sort(temp);
+            for (int j = 0; j < temp.length; j++) {
+				System.out.print(temp[j]);
+			}
             
-            for(int j=0; j<temp.length; j++){
-                if(temp[j] == commands[i][2]){
-                    answer[i] = temp[j];
-                }
-            }
+            System.out.println();
+            
+            int set = commands[i][2];
+            
+            answer[i] = temp[set-1];
+             
             
         }
         
