b0d4a4c7e18f0348574c7b09ac1fab3508b13d0e
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-05-07 22:48:54 +0900

    카카오 인턴십 2020_보석 쇼핑

---
+++ src/Kakao/internship2020_3.java
@@ -0,0 +1,36 @@
+package Kakao;
+
+import java.util.ArrayList;
+
+public class internship2020_3 {
+
+	public static void main(String[] args) {
+		
+		String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
+	
+	
+		 int[] result = new Solution3().solution(gems);	
+		
+		 for (int i = 0; i < result.length; i++) {
+				System.out.print(result[i]);
+		 }
+	
+	
+	}
+
+}
+
+class Solution3 {
+    
+    public int[] solution(String[] gems) {
+    	 int[] answer = {};
+         return answer;
+    	
+    	
+	}
+    
+	
+	
+}
+
+
