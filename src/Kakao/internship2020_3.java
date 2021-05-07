432860b7325a03bdd5f07cc8645bdfa89d427363
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-05-08 00:06:36 +0900

    카카오 인턴십 2020 보석쇼핑

--- src/Kakao/internship2020_3.java
+++ src/Kakao/internship2020_3.java
@@ -1,36 +1,114 @@
 package Kakao;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class internship2020_3 {
 
 	public static void main(String[] args) {
-		
-		String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
-	
-	
-		 int[] result = new Solution3().solution(gems);	
-		
-		 for (int i = 0; i < result.length; i++) {
-				System.out.print(result[i]);
-		 }
-	
-	
+
+		// gems를 탐색하면서 하나씩 다른 객체에 저장하며 모든 종류의 gems이 포함되었는지 체크하자
+
+		String[] gems = { "DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA" };
+
+		int[] result = new Solution3().solution(gems);
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i]);
+		}
+
 	}
 
 }
 
 class Solution3 {
-    
-    public int[] solution(String[] gems) {
-    	 int[] answer = {};
-         return answer;
-    	
-    	
+
+	private static Set<String> gemType = new HashSet<>(); //순서가 중요하지 않고 빠른 검색이 요구되었기 때문에 HashSet 사용
+	private static Queue<String> gemsToBuy = new LinkedList<>(); //a부터 b까지의 gems를 순서대로 저장해야 했기 때문에 Queue 사용
+	private static Map<String, Integer> gemsToBuyCount = new HashMap<>(); //Queue에 저장된 각 gems들의 count를 저장해야 했기 때문에 HashMap 사용
+	private static int startIndex;
+
+	private static int answerStartIndex;
+	private static int answerSize = Integer.MAX_VALUE;
+
+	public int[] solution(String[] gems) {
+		//각기 다른 보석타입구하기
+		countGemType(gems);
+		
+		//
+		shopGems(gems);
+		return new int[] { answerStartIndex + 1, answerStartIndex + answerSize };
+
 	}
-    
+
+	//각기 다른 보석타입 갯수 구하기
+	private void countGemType(String[] gems) {
+		gemType.addAll(Arrays.asList(gems));//일반배열을 arrayList로 변경
+
+	}
 	
+	private void shopGems(String[] gems) {
+		for (String gem : gems) {
+			//1. 구매한 보석에 대한 함수
+			addGemsToBuy(gem);
+			//2. 중복제거
+			removeDuplicateStartGem();
+			
+			updateAnswer();
+		}
+
+	}
+
+	private void addGemsToBuy(String gem) {
+		//나열된 순서대로 구매
+		gemsToBuy.add(gem);
+		updateGemsToBuyCount(gem);
+
+	}
 	
+	//보석을 몇개를 샀는지 카운팅하기
+	private void updateGemsToBuyCount(String gem) {
+		//Map타입에 저장되어 있다면 갯수를 추가하기
+		//저장되어 있지 않으면 값을 추가하기
+		if (gemsToBuyCount.containsKey(gem)) {
+			gemsToBuyCount.put(gem, gemsToBuyCount.get(gem) + 1); //["DIA", 2]
+		} else {
+			gemsToBuyCount.put(gem, 1); //["DIA", 1]
+		}
+
+	}
+	
+	private void removeDuplicateStartGem() {
+		while (true) {
+			String startGem = gemsToBuy.peek(); // 큐에 저장된 순서대로 추출
+			int startGemCount = gemsToBuyCount.get(startGem); //큐에서 꺼낸 보석을 몇번이나 샀는지에 관한 카운트
+			
+			//한번이상 구매를 했다면
+			if (startGemCount > 1) {
+				gemsToBuyCount.put(startGem, startGemCount - 1);
+				gemsToBuy.poll();
+				startIndex++;
+				System.out.println("startIndex"+startIndex);
+			} else {
+				break;
+			}
+		}
+
+	}
+	
+	private void updateAnswer() {
+		if (gemsToBuyCount.size() == gemType.size() && answerSize > gemsToBuy.size()) {
+			answerStartIndex = startIndex;
+			answerSize = gemsToBuy.size();
+		}
+
+	}
+
+	
+
 }
-
-
