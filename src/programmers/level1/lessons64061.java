bb60567926729753218bf93487f3a4aca04c7549
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-06-06 22:53:19 +0900

    프로그래머스_2019카카오개발자겨울인턴십_크레인인형뽑기게임

--- src/programmers/level1/lessons64061.java
+++ src/programmers/level1/lessons64061.java
@@ -1,87 +1,40 @@
 package programmers.level1;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class lessons64061 {
+	public int solution(int[][] board, int[] moves) {
+		int answer = 0;
 
-	
-	static int[] basket;
-	static int count=0;
-	
-	public static void main(String[] args) {
-		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-		
-		int[] moves = {1,5,3,5,1,2,1,4};
-		
-		int res = solution(board, moves);
-		
-		System.out.print(res);
-	}
-	
-	public static int solution(int[][] board, int[] moves) {
-		
-		basket = new int[moves.length];
-		
-        int answer = 0;
-        
-        for (int i = 0; i < moves.length; i++) {
-			
-        	fingDoll(board,moves[i]);
-        	
-		}
-        
-        answer = removeDoll(basket);
-        
-        return answer;
-    }
+		//크레인이 뽑을 인형을 받을 stack
+		Stack<Integer> stack = new Stack<>();
 
-	//사라진 인형 갯수 구하기
-	private static int removeDoll(int[] basket) {
-		
-		int notSameCount =1;
-		
-		for (int i = 1; i < basket.length; i++) {
-			
-			if(basket[i-1] != basket[i]) {
-				notSameCount++;
-			}
-			
-		}
-		
-		int[] temp;
-		temp = new int[notSameCount];
-		int basketIndex = 1;
-		temp[0] = basket[0];
-		
-		for (int i = 1; i < basket.length; i++) {
-			if(basket[i-1] != basket[i]) {
-				temp[basketIndex] = basket[i];
-				basketIndex ++;
-			} else count++;
-			
-		}
-		
-		
-		return count*2;
-	}
+		//stack은 맨 위 값과 비교하는데, 아무것도 없을시 오류하는 것을 방지하기 위해 0을 넣어줌
+		stack.push(0);
 
-	/** 크레인이 집는 인형 찾기*/
-	private static void fingDoll(int[][] board, int n) {
-		
-		for (int i = 0; i < board.length; i++) {
-			//아무것도 없으면 패스
-			if(board[i][n-1] == 0) continue; 
-			
-			else {
-				//바구니에 인형 담기
-				basket[i] = board[i][n-1];
-				
-				//바구니에 넣은 인형자리는 0으로 만들기
-				board[i][n-1] = 0; // 
-				
+		//크레인이 움직이는 만큼 for돌려주기
+		for (int move : moves) {
+			for (int j = 0; j < board.length; j++) {
+
+				//1. 인형이 존재한다면 stack에 넣어주기
+				if (board[j][move - 1] != 0) {
+
+					//이전에 넣은 인형과 같은 인형이라면 삭제해주기
+					if (stack.peek() == board[j][move - 1]) {
+						stack.pop();
+						answer += 2;
+					} else {
+						stack.push(board[j][move - 1]);
+					}
+					
+					//바구니에 넣었다면, 현재 위치는 0으로 만들기
+					board[j][move - 1] = 0;
+					break;
+				}
 			}
 		}
-
+		return answer;
 	}
 
 }
