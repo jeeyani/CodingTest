4d0c0d4594800e8a68cd6eb8060b065ca9693a7c
jeeyani <jeeyani@DESKTOP-HU9K5MN>
2021-06-15 22:01:40 +0900

    프로그래머스 greedy 체육복

--- src/programmers/Greedy/lessons42862.java
+++ src/programmers/Greedy/lessons42862.java
@@ -3,8 +3,65 @@ package programmers.Greedy;
 public class lessons42862 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * int N = 5;
+		 * 
+		 * int[] lost = { 2, 4 }; int[] reserve = { 1, 3, 5 };
+		 */
 
+		int N = 5;
+
+		int[] lost = { 2, 4 };
+		int[] reserve = { 3 };
+
+		int res = solution(N, lost, reserve);
+
+		System.out.print(res);
+
+	}
+
+	private static int solution(int n, int[] lost, int[] reserve) {
+
+		int answer = 0;
+
+		int[] listener = new int[n];
+
+		// 도난당한 학생 찾기
+		for (int i = 0; i < lost.length; i++) {
+			listener[lost[i] - 1] = -1;
+
+		}
+		// 여벌가진 학생 찾기
+		for (int i = 0; i < reserve.length; i++) {
+			listener[reserve[i] - 1] = 1;
+		}
+
+		for (int i = 0; i < listener.length; i++) {
+			if (listener[i] < 0) {
+				if (i != 0 && listener[i-1] > 0) {
+					listener[i]++;
+					listener[i -1]--;
+
+				} else if (i != n - 1 && listener[i+1] > 0) {
+					listener[i]++;
+					listener[i + 1]--;
+				}
+			}
+
+		}
+
+		for (int i = 0; i < listener.length; i++) {
+			if (!(listener[i] < 0)) {
+				answer++;
+			}
+		}
+
+		// 수업듣고 있는 사람 확인 용
+		for (int i = 0; i < listener.length; i++) {
+			System.err.println(listener[i]);
+		}
+
+		return answer;
 	}
 
 }
