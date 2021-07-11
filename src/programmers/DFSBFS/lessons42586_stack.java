0bd265c0ee7536e0843183a58c40b221362c0bad
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-07-11 22:33:35 +0900

    프로그래머스 기능개발_미완

---
+++ src/programmers/DFSBFS/lessons42586_stack.java
@@ -0,0 +1,23 @@
+package programmers.DFSBFS;
+
+public class lessons42586_stack {
+
+	public static void main(String[] args) {
+		
+		int[] progresses = {93,30,55};
+		int[] speeds = {1,30,5};
+		
+		int[] res = solution(progresses, speeds);
+		
+		for (int i = 0; i < res.length; i++) {
+			System.out.print(res[i]+" ");
+		}
+
+	}
+
+	private static int[] solution(int[] progresses, int[] speeds) {
+		int[] answer = {};
+        return answer;
+	}
+
+}
