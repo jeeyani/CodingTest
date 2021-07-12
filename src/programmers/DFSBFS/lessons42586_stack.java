01b6848c48405597b4f71901912b3b155301dddf
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-07-12 23:41:48 +0900

    프로그래머스 기능개발

--- src/programmers/DFSBFS/lessons42586_stack.java
+++ src/programmers/DFSBFS/lessons42586_stack.java
@@ -1,5 +1,7 @@
 package programmers.DFSBFS;
 
+import java.util.*;
+
 public class lessons42586_stack {
 
 	public static void main(String[] args) {
@@ -16,8 +18,50 @@ public class lessons42586_stack {
 	}
 
 	private static int[] solution(int[] progresses, int[] speeds) {
-		int[] answer = {};
-        return answer;
+		List<Integer> answer = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        // insert first Element
+        int tempMax = calculateRemain(progresses[0], speeds[0]);
+        stack.push(tempMax);
+
+        for (int i = 1; i < progresses.length; i++) {
+            // 남은 remain
+            int remain = calculateRemain(progresses[i], speeds[i]);
+
+            // 핵심은 만약에 peek보다 remain이 더 크면 이전 까지 다 함께 배포해야함.
+            if (tempMax < remain) {
+                int unit = 0;
+                // 스택 갯수만큼 유닛이 쌓임.
+                while (!stack.isEmpty()) {
+                    unit += 1;
+                    stack.pop();
+                }
+                tempMax = remain;
+                answer.add(unit);
+            }
+            // 매번 스택에 푸쉬 하는건 맞음. 대신에
+            stack.push(remain);
+        }
+
+        // 남은 스택이 있다면 추가로 작업
+        if (!stack.isEmpty()) {
+            int unit = 0;
+            while (!stack.isEmpty()) {
+                unit += 1;
+                stack.pop();
+            }
+            answer.add(unit);
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static int calculateRemain(int progress, int speed) {
+        int remain = (100 - progress) / speed;
+        if ((100 - progress) % speed != 0) {
+            return remain + 1;
+        }
+        return remain;
 	}
 
 }
