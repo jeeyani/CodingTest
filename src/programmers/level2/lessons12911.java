b9466101c7570a8f59ae266f741d0a28803ba136
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-08-24 22:06:04 +0900

    프로그래머스 다음 큰 수

--- src/programmers/level2/lessons12911.java
+++ src/programmers/level2/lessons12911.java
@@ -6,21 +6,30 @@ public class lessons12911 {
 		int n = 78;
 		
 		int res = solution(n);
- 
+		res = solution2(n);
+		System.out.println(res);
+	}
+
+	/*다른 사람 풀이*/
+	//비트연사자를 활용한 풀이
+	
+	private static int solution2(int n) {
+		int postPattern = n & -n, smallPattern = ((n ^ (n + postPattern)) / postPattern) >> 2;
+        return n + postPattern | smallPattern;
 	}
 
 	private static int solution(int n) {
-int answer = 0;
+		int answer = 0;
         
         String nBinary ="";
-        nBinary = Integer.toBinaryString(n);
+        nBinary = Integer.toBinaryString(n); //toBinaryString 이진수 변환
     
         int nOneCount = oneCOunt(nBinary);
         
         int nextNcount =0;
         int nNext = n+1;
         
-        while(nextNcount != nOneCount){
+        while(true){
             
             nBinary =  Integer.toBinaryString(nNext);
             nextNcount = oneCOunt(nBinary);
