4dc6348aa74b3bdf8c5595133088c5119f063174
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-08-26 22:18:14 +0900

    프로그래머스 위클리 챌린지 4주차_직업군 추천하기

---
+++ src/programmers/level1/lessons84325.java
@@ -0,0 +1,73 @@
+package programmers.level1;
+
+public class lessons84325 {
+
+	public static void main(String[] args) {
+		String[] table = { "SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
+				"HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP",
+				"GAME C++ C# JAVASCRIPT C JAVA" };
+		String[] languages = { "PYTHON", "C++", "SQL" };
+		int[] preference = { 7, 5, 5 };
+
+		String result;
+
+		result = solution(table, languages, preference);
+		System.out.println(result);
+
+	}
+
+
+	private static String solution(String[] table, String[] languages, int[] preference) {
+		String answer = "";
+
+		int[] sum = new int[5];
+
+		//1. table과 languages와 비교
+		for (int i = 0; i < languages.length; i++) {
+
+			for (int j = 0; j < 5; j++) {
+				int index = 0;
+				String[] tempSplit = table[j].split(" ");
+
+				for (String str : tempSplit) {
+					index++;
+
+					if (languages[i].equals(str)) {
+						sum[j] += cal(index) * preference[i];
+					}
+
+				}
+			}
+		}
+		int max =0;
+		int index = 0;
+		for (int i = 0; i < sum.length; i++) {
+			if(max < sum[i]) {
+				max = sum[i];
+				index = i;
+			}
+		}
+		String[] chk = table[index].split(" ");
+		answer = chk[0];
+		
+		return answer;
+	}
+
+	private static int cal(int index) {
+		switch (index) {
+		case 2:
+			return 5;
+		case 3:
+			return 4;
+		case 4:
+			return 3;
+		case 5:
+			return 2;
+		case 6:
+			return 1;
+		default:
+			return 0;
+		}
+	}
+
+}
